49a9fb2866c339f212416cc8ede11e7395f6bd66
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-04-28 00:04:12 +0200

    Adding pvmake

--- tool/PvMake/Program.cs
+++ tool/PvMake/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PvMake
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
