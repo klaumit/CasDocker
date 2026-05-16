c5156c62270b61239bfa1efbc5e763974fb957f3
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-05-17 01:28:22 +0200

    Adding files

---
+++ src/PvMake/Program.cs
@@ -0,0 +1,32 @@
+﻿using CommandLine;
+using PvMake.Core;
+
+namespace PvMake
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var parser = Parser.Default;
+            parser.ParseArguments<Options>(args).WithParsed(o =>
+            {
+                if (o.Clean)
+                {
+                    Cleaner.Run(o);
+                }
+                else if (o.Build)
+                {
+                    Builder.Run(o);
+                }
+                else if (o.Simulate)
+                {
+                    Simulator.Run(o);
+                }
+                else if (o.Upload)
+                {
+                    Uploader.Run(o);
+                }
+            });
+        }
+    }
+}
