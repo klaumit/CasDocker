abc8a07f22a25ca01e3157521fe2374b96303dc6
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-05-17 02:34:46 +0200

    Moving code

--- src/PvMake/Core/Simulator.cs
+++ src/PvMake/Core/Simulator.cs
@@ -25,23 +25,25 @@ namespace PvMake.Core
                 Console.WriteLine(" * Starting {0} of {1}...", lbl, sdk);
                 if (isHitachi)
                 {
-                    var regMark = "vb6.txt";
-                    var regMarkF = Path.Combine(sdkDir, regMark);
-                    var reg = File.Exists(regMarkF);
-                    if (!reg)
-                    {
-                        var ocxs = FileExt.Find(sdkDir, "*.OCX").OrderBy(x => x).ToArray();
-                        foreach (var ocx in ocxs)
-                        {
-                            ProcExt.Start("regsvr32", sdkDir, ocx);
-                        }
-                        File.WriteAllText(regMarkF, "done");
-                    }
+                    RegVb(sdkDir);
                 }
                 ProcExt.Start(exe, sdkDir, null);
             }
 
             Console.WriteLine("Done.");
+        }
+
+        private static void RegVb(string sdkDir)
+        {
+            const string regMark = "vb6.txt";
+            var regMarkF = Path.Combine(sdkDir, regMark);
+            var reg = File.Exists(regMarkF);
+            if (reg)
+                return;
+            var ocxs = FileExt.Find(sdkDir, "*.OCX").OrderBy(x => x).ToArray();
+            foreach (var ocx in ocxs)
+                ProcExt.Start("regsvr32", sdkDir, ocx);
+            File.WriteAllText(regMarkF, "done");
         }
     }
 }
