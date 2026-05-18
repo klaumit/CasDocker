1b52b6cfb3eda6d32f7ecf3f9b83196f746ea0e0
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-05-19 00:22:31 +0200

    Creating even empty dirs

--- src/PvMake/Core/Builder.cs
+++ src/PvMake/Core/Builder.cs
@@ -29,11 +29,18 @@ namespace PvMake.Core
         private static void PrepareIntel(string sdkDir, Project proj)
         {
             var cDir = FileExt.GetDir(Path.Combine(sdkDir, "C"), false);
+            var cuDir = FileExt.GetDir(Path.Combine(cDir, "User_Bin"), true);
             var pDir = FileExt.GetDir(Path.Combine(cDir, proj.AppName), true);
 
             var zipFile = Path.Combine(B.archRepo, "LSIJ_proj" + ".tar.gz");
             ZipExt.Uncompress(zipFile, pDir);
 
+            var ccDir = FileExt.GetDir(Path.Combine(pDir, "C"), true);
+            var fdDir = FileExt.GetDir(Path.Combine(pDir, "ForDEBUG"), true);
+            var hhDir = FileExt.GetDir(Path.Combine(pDir, "H"), true);
+            var miDir = FileExt.GetDir(Path.Combine(pDir, "MENUICON"), true);
+            var obDir = FileExt.GetDir(Path.Combine(pDir, "OBJ"), true);
+            
             var mFile = Path.Combine(pDir, "Makefile");
             FileExt.WriteWin(mFile, CreateMakeFile(proj,
                 new List<string>(), new List<string>()));
