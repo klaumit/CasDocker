e38e2e572a8e21ef307a3ddef4540d74dc144b84
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-05-19 22:25:39 +0200

    Replacing lines

--- src/PvMake.Lib/WriteExt.cs
+++ src/PvMake.Lib/WriteExt.cs
@@ -35,18 +35,54 @@ namespace PvMake.Lib
             }
         }
 
+        private static IDictionary<string, string> GetSymbols(bool patchHit)
+        {
+            var symbols = new Dictionary<string, string>();
+            if (patchHit)
+            {
+                symbols["FAR"] = " ";
+                symbols["PADE"] = ",\t0x00";
+                symbols["B@@"] = "(byte)";
+                symbols["C@"] = "(char*)";
+                symbols["B@"] = "(byte *)";
+                symbols["FF@"] = " 0xffffffff";
+            }
+            else
+            {
+                symbols["FAR"] = " far ";
+                symbols["PADE"] = "";
+                symbols["B@@"] = "";
+                symbols["C@"] = "";
+                symbols["B@"] = "";
+                symbols["FF@"] = " 0xffff";
+            }
+            return symbols;
+        }
+
         public static void ReWrite(IEnumerable<string> files, string dest, bool patchHit, string root)
         {
             if (files == null)
                 return;
+            var symbols = GetSymbols(patchHit);
             foreach (var file in files)
             {
                 var name = Path.GetFileName(file);
                 var tgt = Path.Combine(dest, name);
                 var lines = new List<string>();
-                using (var input = new StreamReader(file, Encoding.ASCII))
+                string input;
+                using (var reader = new StreamReader(file, Encoding.ASCII))
+                    input = reader.ReadToEnd();
+                string tmp;
+                foreach (var iLine in input.Split('\n'))
                 {
-
+                    var line = iLine.TrimEnd();
+                    foreach (var (key, val) in symbols)
+                    {
+                        tmp = $" {key} ";
+                        if (line.Contains(tmp))
+                            line = line.Replace(tmp, val);
+                    }
+                    lines.Add(line);
                 }
                 FileExt.WriteWin(tgt, lines);
                 var lTgt = Path.GetFullPath(tgt).Replace(root, ".");
