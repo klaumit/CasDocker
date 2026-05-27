2614b1459b297e72a2f714d3aac5df5f110ea8ea
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-05-27 23:56:49 +0200

    Fixed waiting period

--- apps/Hello/src/hello.c
+++ apps/Hello/src/hello.c
@@ -1,21 +1,31 @@
 #include <define.h>
 #include <libc.h>
 #include <l_libc.h>
+#include <stdio.h>
+#include <string.h>
 
 void main()
 {
+	char debug[128];
+	int i;
+
 	LibInitDisp();
+	LibClrDisp();
+	
 	LibStringDsp( B@ "Hello World!!!", 5, 10, 160, B@@ IB_PFONT2);
 	LibStringDsp( B@ "Your device is fucked", 5, 30, 160, B@@ IB_PFONT1);
 	LibStringDsp( B@ "by the mafia", 5, 40, 160, B@@ IB_PFONT2);
 	LibStringDsp( B@ "http://www.mafia.com.uk", 5, 60, 160, B@@ IB_PFONT2);
 	LibStringDsp( B@ "admin@nonsense.com.uk", 5, 70, 160, B@@ IB_PFONT2);
 	LibPutDisp();
-	LibWait(IB_1SWAIT);
-	LibWait(IB_1SWAIT);
-	LibWait(IB_1SWAIT);
-	LibWait(IB_1SWAIT);
-	LibWait(IB_1SWAIT);
-	LibWait(IB_1SWAIT);
+
+	for (i = 0; i < 10000; i++)
+	{
+		sprintf(debug, "Waiting %d sec...", i);
+		LibStringDsp( B@ debug, 5, 100, 160, B@@ IB_PFONT2);
+		LibPutDisp();
+		LibWait(IB_1SWAIT);
+	}	
 	LibJumpMenu();
 }
+
