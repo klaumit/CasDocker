89faebb89e24579e73d12dd3e387994b4b4fadc0
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-05-28 23:35:02 +0200

    Working on link

--- apps/MemLink/src/main.c
+++ apps/MemLink/src/main.c
@@ -4,11 +4,38 @@
 #include <stdio.h>
 #include <string.h>
 
+void wait()
+{
+	LibWait(IB_1SWAIT);
+}
+
+void writeln(char *txt, int x, int y)
+{
+	LibStringDsp( B@ txt, x, y, 160, B@@ IB_CG57FONT);
+	LibPutDisp();
+}
+
 void main()
 {
+	char tmp[128];
+	int i = 0;
+	int z = 0;
+
 	LibInitDisp();
 	LibClrDisp();
 
+	while (1)
+	{
+		if (z >= 160)
+			z = 0;
+		else
+			z = z + 7;
+
+		i = i + 1;
+		sprintf(tmp, "%d %d %d", i);
+		writeln(tmp, 0, z);
+		wait();
+	}
+
 	LibJumpMenu();
 }
-
