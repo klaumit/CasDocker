1e62632d25f12f63c910a4f3361d2e68fbe08028
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-05-29 21:53:28 +0200

    Setting up structure

---
+++ src/MemForge/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace xxyy
+{
+    internal static class Program
+    {
+        private static void Main2(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
