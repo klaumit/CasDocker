ce1eb5a6c85475daad495b3783f962a196657c91
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-06-11 00:03:36 +0200

    Moving files

--- src/Pva2cpa/Program.cs
+++ src/Pva2cpa/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace Pva2cpa
 {
     internal static class Program
     {
+        [STAThread]
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
