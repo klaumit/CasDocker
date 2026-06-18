3a791a86130092bd6b444a2f9c185795ebb65d97
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-06-18 20:29:09 +0200

    Adding it

--- src/PvBake/Program.cs
+++ src/PvBake/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace PvBake
+using System;
+using CommandLine;
+using PvMake.Core;
+
+namespace PvMake
 {
     internal static class Program
     {
         private static void Main(string[] args)
         {
-            SimHandler.ExtractSim();
+            var parser = Parser.Default;
+            parser.ParseArguments<Options>(args).WithParsed(o =>
+            {
+                try
+                {
+                    RunAll(o);
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine(" [ERROR] {0}", ex.Message);
+                }
+            });
+        }
+
+        private static void RunAll(Options o)
+        {
+            if (o.Clean)
+            {
+                Cleaner.Run(o);
+            }
         }
     }
 }
