b694db09fb5e5a7254d1d6f94fe5a1ba47fe6b90
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-06-21 15:48:38 +0200

    Adding icon length

--- src/PvBake.Lib/Models/Icon.cs
+++ src/PvBake.Lib/Models/Icon.cs
@@ -5,6 +5,7 @@ namespace PvBake.Lib.Models
         public ushort Width { get; set; }
         public ushort Height { get; set; }
         public bool[] Pixels { get; set; }
+        public uint Length => (uint)(4 + ((Width + 7) / 8) * Height);
 
         public bool GetPixel(int x, int y) => Pixels[y * Width + x];
     }
