c83ea6df81e1e69e7ce30406f767e12cdfefe4ed
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-06-21 19:06:01 +0200

    Writing pic

--- src/PvBake.Lib/Core/Icons.cs
+++ src/PvBake.Lib/Core/Icons.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Text;
 using PvBake.Lib.Models;
@@ -28,51 +27,6 @@ namespace PvBake.Lib.Core
             b.Write(a.Height);
             var rowLen = a.GetRowBytes();
             b.Write(FromPixelArray(a.Pixels, a.Width, a.Height, rowLen));
-            return true;
-        }
-
-        public static bool SaveX86Bmp(Icon a, Stream stream)
-        {
-            var enc = Encoding.ASCII;
-            using var b = new BinaryWriter(stream, enc);
-            int width = a.Width;
-            int height = a.Height;
-            var unpaddedRowBytes = (width + 7) / 8;
-            var rowBytes = (unpaddedRowBytes + 3) & ~3;
-            const int paletteSize = 2 * 4;
-            var pixelDataSize = rowBytes * height;
-            const int fileHeaderSize = 14;
-            const int infoHeaderSize = 40;
-            const int pixelDataOffset = fileHeaderSize + infoHeaderSize + paletteSize;
-            var fileSize = pixelDataOffset + pixelDataSize;
-            b.Write((byte)'B');
-            b.Write((byte)'M');
-            b.Write(fileSize);
-            b.Write((short)0);
-            b.Write((short)0);
-            b.Write(pixelDataOffset);
-            b.Write(infoHeaderSize);
-            b.Write(width);
-            b.Write(height);
-            b.Write((short)1);
-            b.Write((short)1);
-            b.Write(0);
-            b.Write(pixelDataSize);
-            b.Write(3780);
-            b.Write(3780);
-            b.Write(0);
-            b.Write(0);
-            b.Write(new byte[] { 0x00, 0x00, 0x00, 0x00 });
-            b.Write(new byte[] { 0xFF, 0xFF, 0xFF, 0x00 });
-            var row = new byte[rowBytes];
-            for (var y = height - 1; y >= 0; y--)
-            {
-                Array.Clear(row, 0, row.Length);
-                for (var x = 0; x < width; x++)
-                    if (!a.GetPixel(x, y))
-                        row[x >> 3] |= (byte)(0x80 >> (x & 7));
-                b.Write(row);
-            }
             return true;
         }
 
