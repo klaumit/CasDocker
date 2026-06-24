b93c084dce98207c30ddbb04d5002dde0f5d7299
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-06-24 19:40:33 +0200

    Adding it

---
+++ apps/PocLink/src/hello.c
@@ -0,0 +1,33 @@
+#include <define.h>
+#include <libc.h>
+#include <l_libc.h>
+#include <stdio.h>
+#include <string.h>
+#include "sysdm.h"
+
+void main()
+{
+	char debug[128];
+	int i;
+
+	LibInitDisp();
+	LibClrDisp();
+	
+	LibStringDsp( B@ "### Pocket Link ###", 5, 10, 160, B@@ IB_PFONT2);
+
+	sprintf(debug, "CPU architecture : %s", GetCpuStr(GetCpuKind()));
+	LibStringDsp( B@ debug, 5, 30, 160, B@@ IB_PFONT1);
+	sprintf(debug, "Communication    : %s", GetCommStr(GetCommKind()));
+	LibStringDsp( B@ debug, 5, 40, 160, B@@ IB_PFONT1);
+
+	LibPutDisp();
+
+	for (i = 0; i < 100; i++)
+	{
+		sprintf(debug, "Waiting %d sec...", i);
+		LibStringDsp( B@ debug, 5, 100, 160, B@@ IB_PFONT2);
+		LibPutDisp();
+		LibWait(IB_1SWAIT);
+	}
+	LibJumpMenu();
+}
