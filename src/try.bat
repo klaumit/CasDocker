3e07581ec3e82dc3906b55161ac1145ac6a4f613
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-06-24 18:13:41 +0200

    Skipping it

--- src/try.bat
+++ src/try.bat
@@ -3,12 +3,13 @@
 call build.bat
 
 set EXE=PvMake\bin\Debug\fx40\PvMake
-set PRJ=Y:\apps\MemLink
+set PRJ=Y:\apps\Hello
 
 echo.
 echo  ::: Prepare :::
 echo.
-%EXE% --clean --prepare --build --simulate -i %PRJ%
+REM echo --simulate 
+%EXE% --clean --prepare --build -i %PRJ%
 
 echo 
 
