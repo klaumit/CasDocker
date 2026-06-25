df2ed70ea31a1a0eb49b60979a559b1dda6f9e38
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-06-25 23:57:57 +0200

    Working on it

--- src/DevForge.Lib/Legacy/LegacyDevice.cs
+++ src/DevForge.Lib/Legacy/LegacyDevice.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading;
 using DevForge.Lib.API;
 using System.IO.Ports;
 
@@ -5,16 +7,27 @@ namespace DevForge.Lib.Legacy
 {
     public sealed class LegacyDevice : ICommDevice
     {
+        private Thread _thread;
         private SerialPort _port;
 
         public void Start()
         {
+            _thread = new Thread(DoLoop) { IsBackground = true, Name = "Loop" };
+            _thread.Start();
+        }
+
+        private void DoLoop()
+        {
             _port = Serials.CreatePort();
+            var head = _port.ReadString();
+            Console.WriteLine(" " + this.GetType().Name + " => '" + head + "'");
         }
 
         public void Stop()
         {
             Serials.ClosePort(ref _port);
+            _thread.Interrupt();
+            _thread.Abort();
         }
 
         public void Dispose()
--- src/DevForge.Lib/Modern/ModernDevice.cs
+++ src/DevForge.Lib/Modern/ModernDevice.cs
@@ -1,19 +1,33 @@
+using System;
+using System.Threading;
 using DevForge.Lib.API;
+using System.IO.Ports;
 
 namespace DevForge.Lib.Modern
 {
     public sealed class ModernDevice : ICommDevice
     {
+        private Thread _thread;
         private UsbPort _port;
 
         public void Start()
         {
+            _thread = new Thread(DoLoop) { IsBackground = true, Name = "Loop" };
+            _thread.Start();
+        }
+
+        private void DoLoop()
+        {
             _port = Universals.CreatePort();
+            var head = _port.ReadString();
+            Console.WriteLine(" " + this.GetType().Name + " => '" + head + "'");
         }
 
         public void Stop()
         {
             Universals.ClosePort(ref _port);
+            _thread.Interrupt();
+            _thread.Abort();
         }
 
         public void Dispose()
