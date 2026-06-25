ca425e062e3e72461c494f04288bdbc6ba6d4f23
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-06-26 00:37:04 +0200

    Fixed

--- src/DevForge.Lib/Legacy/Serials.cs
+++ src/DevForge.Lib/Legacy/Serials.cs
@@ -29,7 +29,7 @@ namespace DevForge.Lib.Legacy
             var bytesRead = port.Read(buffer, 0, buffer.Length);
             int rest;
             while ((rest = port.BytesToRead) >= 1)
-                bytesRead = port.Read(buffer, bytesRead, buffer.Length - bytesRead);
+                bytesRead += port.Read(buffer, bytesRead, buffer.Length - bytesRead);
             if (bytesRead < 1)
                 return null;
             var text = Encoding.ASCII.GetString(buffer, 0, bytesRead);
