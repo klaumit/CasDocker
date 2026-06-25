9be208589471d4efb141aa72cb9746f10dd99aed
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-06-25 20:08:34 +0200

    Creating a serial port version

--- src/DevForge.Lib/Legacy/Class1.cs
+++ src/DevForge.Lib/Legacy/Class1.cs
@@ -1,8 +1,25 @@
+using System.Linq;
 using DevForge.Lib.API;
+using System.IO.Ports;
 
 namespace DevForge.Lib.Legacy
 {
     public sealed class LegacyCommDevice : ICommDevice
     {
+        private SerialPort _port;
+
+        public LegacyCommDevice()
+        {
+            var names = SerialPort.GetPortNames();
+            var name = names.Last();
+            var speed = 38400;
+            var parity = Parity.None;
+            var dataB = 8;
+            var stopB = StopBits.One;
+            var port = new SerialPort(name, speed, parity, dataB, stopB);
+            port.Handshake = Handshake.RequestToSend;
+            port.Open();
+            _port = port;
+        }
     }
 }
