62235f88c1d12ea5d8e60cbc4cdceaca4537923b
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-06-25 19:40:10 +0200

    Moving

--- src/DevForge/Program.cs
+++ src/DevForge/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using DevForge.Lib;
 
 namespace DevForge
 {
@@ -9,6 +10,8 @@ namespace DevForge
     {
         static void Main(string[] args)
         {
+            Class1 c;
+
         }
     }
 }
