014e1bbb120bfb80030bec42f9276df5f24ae68d
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-06-25 20:17:37 +0200

    Adding native wrapper

---
+++ src/DevForge.Lib/Modern/EnumDevNative.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace DevForge.Lib.Modern
+{
+    internal static class EnumDevNative
+    {
+        private const string DllName = "EnumDev.dll";
+
+        [DllImport(DllName, EntryPoint = "_PVEnumUsbA@12", CallingConvention = CallingConvention.StdCall)]
+        public static extern int PVEnumUsbA(int deviceIndex,
+            [MarshalAs(UnmanagedType.LPArray)] byte[] outDevicePath,
+            int bufferMaxLen);
+
+        [DllImport(DllName, EntryPoint = "_PVReadUsb@16", CallingConvention = CallingConvention.StdCall)]
+        public static extern bool PVReadUsb(IntPtr deviceHandle,
+            byte[] outBuffer, uint length, out uint bytesRead);
+
+        [DllImport(DllName, EntryPoint = "_PVWriteUsb@16", CallingConvention = CallingConvention.StdCall)]
+        public static extern bool PVWriteUsb(IntPtr deviceHandle,
+            byte[] inBuffer, uint length, out uint bytesWritten);
+
+        private const string KernName = "kernel32.dll";
+
+        [DllImport(KernName, SetLastError = true, CharSet = CharSet.Ansi)]
+        public static extern IntPtr CreateFile(string lpFileName, uint dwDesiredAccess, uint dwShareMode,
+            IntPtr lpSecurityAttributes, uint dwCreationDisposition, uint dwFlagsAndAttributes, IntPtr hTemplateFile);
+    }
+}
