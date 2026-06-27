90c3b0f90479fc5132e3bb1e152e08b807318d0a
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-06-27 15:02:53 +0200

    Moving code

---
+++ src/DevForge.Lib/Modern/Internals/KernelNative.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace DevForge.Lib.Modern
+{
+    internal static class KernelNative
+    {
+        private const string KernName = "kernel32.dll";
+
+        public const uint GENERIC_READ_WRITE = 0xC0000000;
+        public const int OPEN_EXISTING = 3;
+        public const int FILE_FLAG_OVERLAPPED = 0x40000000;
+
+        [DllImport(KernName, SetLastError = true, CharSet = CharSet.Ansi)]
+        public static extern IntPtr CreateFile(string lpFileName, uint dwDesiredAccess, uint dwShareMode,
+            IntPtr lpSecurityAttributes, uint dwCreationDisposition, uint dwFlagsAndAttributes, IntPtr hTemplateFile);
+
+        [DllImport(KernName, SetLastError = true)]
+        public static extern bool CloseHandle(IntPtr handle);
+    }
+}
