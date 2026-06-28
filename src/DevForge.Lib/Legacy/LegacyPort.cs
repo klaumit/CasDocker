b9ebb78ea9e8962cbf9582c4427c8aa38dcbdb47
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-06-28 20:44:33 +0200

    Removing

--- src/DevForge.Lib/Legacy/LegacyPort.cs
+++ src/DevForge.Lib/Legacy/LegacyPort.cs
@@ -40,54 +40,18 @@ namespace DevForge.Lib.Legacy
         {
             if (_port == null)
                 return null;
-
-            Console.WriteLine("1| "+count);
-            
             var buffer = new byte[count];
-            
-            Console.WriteLine("2| "+buffer.Length);
-            
             int bytesRead = 0;
-            
-            Console.WriteLine("3| "+bytesRead);
-            
             while (bytesRead < count)
             {
                 int got = _port.Read(buffer, bytesRead, count - bytesRead);
-                
-                Console.WriteLine("3b| "+bytesRead+" "+count+" "+got);
-                
                 if (got <= 0) break;
-                
-                Console.WriteLine("3c| "+bytesRead+" "+count+" "+got);
-                
                 bytesRead += got;
-                
-                Console.WriteLine("3d| "+bytesRead+" "+count+" "+got);
             }
-            
-            
-            /*
-            int rest;
-            while ((bytesRead < buffer.Length) && (rest = _port.BytesToRead) >= 1)
-            {
-                Console.WriteLine("4| "+bytesRead);
-                
-                bytesRead += _port.Read(buffer, bytesRead, rest);
-            }*/
-            
-            Console.WriteLine("5| "+bytesRead);
-            
             if (bytesRead < 1)
                 return null;
-            
-            Console.WriteLine("6| "+bytesRead);
-            
             if (buffer.Length != bytesRead)
                 Array.Resize(ref buffer, bytesRead);
-            
-            Console.WriteLine("7| "+buffer.Length);
-            
             return buffer;
         }
 
