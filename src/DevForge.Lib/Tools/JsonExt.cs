ddd90f03b7af51ddf327821a67e16b9a52f9fa63
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-07-03 00:08:01 +0200

    Adding code

---
+++ src/DevForge.Lib/Tools/JsonExt.cs
@@ -0,0 +1,34 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace PvMake.Lib
+{
+    public static class JsonExt
+    {
+        private static JsonSerializerSettings GetConfig(bool fmt = true)
+        {
+            var opt = new JsonSerializerSettings
+            {
+                Converters = { new StringEnumConverter() },
+                NullValueHandling = NullValueHandling.Ignore,
+                Formatting = fmt ? Formatting.Indented : Formatting.None
+            };
+            return opt;
+        }
+
+        public static string ToJson<T>(T obj, bool fmt = true)
+        {
+            if (obj == null)
+                return null;
+            return JsonConvert.SerializeObject(obj, GetConfig(fmt));
+        }
+
+        public static T ToObj<T>(string raw)
+        {
+            string text;
+            if ((text = raw.TrimOrNull()) == null)
+                return default;
+            return JsonConvert.DeserializeObject<T>(text, GetConfig());
+        }
+    }
+}
