0598f8e22a5f48021315da8bff1a7919af6740d6
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-07-03 23:42:30 +0200

    Fixing this

--- src/DevForge/Program.cs
+++ src/DevForge/Program.cs
@@ -15,18 +15,18 @@ namespace DevForge
     {
         private static void Main(string[] args)
         {
-            // using (ICommDevice dev1 = new PocketDevice(new LegacyFactory()))
-            using (ICommDevice dev2 = new PocketDevice(new ModernFactory()))
+            using (ICommDevice dev1 = new PocketDevice(new LegacyFactory()))
+            // using (ICommDevice dev2 = new PocketDevice(new ModernFactory()))
             {
-                // dev1.Start();
-                dev2.Start();
+                dev1.Start();
+                // dev2.Start();
 
                 Console.WriteLine("Waiting...");
                 Console.ReadLine();
 
-                var quit = new Quit("Noyb");
-                // dev1.Send(quit);
-                dev2.Send(quit);
+                var quit = new Quit("Fick dich, C# !");
+                dev1.Send(quit);
+                // dev2.Send(quit);
 
                 Console.WriteLine("Sending...");
                 Thread.Sleep(1000);
