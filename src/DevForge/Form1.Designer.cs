5e60f38552573c41a96b3372004a9387bb2d38bf
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-07-04 14:53:43 +0200

    Adding code

---
+++ src/DevForge/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace DevForge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.quitBtn = new System.Windows.Forms.Button();
+            this.imgBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // quitBtn
+            // 
+            this.quitBtn.Location = new System.Drawing.Point(443, 385);
+            this.quitBtn.Name = "quitBtn";
+            this.quitBtn.Size = new System.Drawing.Size(75, 23);
+            this.quitBtn.TabIndex = 0;
+            this.quitBtn.Text = "Exit";
+            this.quitBtn.UseVisualStyleBackColor = true;
+            this.quitBtn.Click += new System.EventHandler(this.quitBtn_Click);
+            // 
+            // imgBox
+            // 
+            this.imgBox.Location = new System.Drawing.Point(120, 94);
+            this.imgBox.Name = "imgBox";
+            this.imgBox.Size = new System.Drawing.Size(228, 168);
+            this.imgBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgBox.TabIndex = 1;
+            this.imgBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 420);
+            this.Controls.Add(this.imgBox);
+            this.Controls.Add(this.quitBtn);
+            this.Name = "Form1";
+            this.Text = "DevForge";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button quitBtn;
+        private System.Windows.Forms.PictureBox imgBox;
+    }
+}
+
---
+++ src/DevForge/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevForge.Resources;
+
+namespace DevForge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Quit()
+        {
+            Environment.Exit(0);
+        }
+
+        private void quitBtn_Click(object sender, EventArgs e)
+        {
+            Quit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var img = ResExt.ToImage(ResExt.GetStream("device.png"));
+            imgBox.Image = img;
+        }
+    }   
+}
