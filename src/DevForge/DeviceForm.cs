eb25915b03882cc78dd06fc67f1cdf3130c027c1
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-07-05 16:47:46 +0200

    Adding

---
+++ src/DevForge/DeviceForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevForge.Lib.Common;
+using DevForge.Resources;
+
+// ReSharper disable ArrangeObjectCreationWhenTypeEvident
+// ReSharper disable LocalizableElement
+
+namespace DevForge
+{
+    public partial class DeviceForm : Form
+    {
+        public DeviceForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Icon = ResExt.GetStream("app.ico").ToIcon();
+        }
+    }
+}
