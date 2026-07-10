9c352967548ee8de8f13465d999ee968e2b5f9cc
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-07-10 21:50:58 +0200

    Adding lines helper

---
+++ src/DevForge.Lib/Tools/JsonLines.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace DevForge.Lib.Tools
+{
+	public sealed class JsonLines : IDisposable
+	{
+		private StreamWriter _writer;
+		private JsonSerializerSettings _config;
+
+		public JsonLines(string path) : this(File.CreateText(path))
+		{
+		}
+
+		public JsonLines(StreamWriter writer)
+		{
+			_writer = writer;
+			_config = new JsonSerializerSettings
+			{
+				Formatting = Formatting.None,
+				Converters = { new StringEnumConverter() },
+				NullValueHandling = NullValueHandling.Include
+			};
+		}
+
+		public void Write(object obj)
+		{
+			var json = JsonConvert.SerializeObject(obj, _config);
+			_writer.WriteLine(json);
+			_writer.Flush();
+		}
+
+		public void Dispose()
+		{
+			_writer.Close();
+			_writer.Dispose();
+		}
+	}
+}
