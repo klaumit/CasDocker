accfbd5c843858eae8ffedab32e35f2235ef4838
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-07-20 23:49:12 +0200

    Searching for the golden place

--- src/MemForge.Lib/MemReader.cs
+++ src/MemForge.Lib/MemReader.cs
@@ -91,10 +91,28 @@ namespace MemForge.Lib
 			{
 				foreach (var item in ReadAll(pid))
 				{
+					/*
 					var array = item.Buffer.SwapEndian(true);
 					var debug = Encoding.ASCII.GetBytes(item.Info.ToStr());
 					outPut.Write(debug, 0, debug.Length);
 					outPut.Write(array, 0, item.Buffer.Length);
+					*/
+
+					if (item.Info.AllocationProtect == 0x00000004 &&
+						item.Info.State == 0x00001000 &&
+						item.Info.Protect == 0x00000004 &&
+						item.Info.Type == 0x00020000 &&
+						item.Info.RegionSize == 0x00085000)
+					{
+						var array = item.Buffer.SwapEndian(true);
+						var text = Encoding.UTF8.GetString(array);
+						if (text.Contains("###MEMORY_MARKER_START0###"))
+						{
+							var debug = Encoding.ASCII.GetBytes(item.Info.ToStr() + "\r\n");
+							outPut.Write(debug, 0, debug.Length);
+							outPut.Write(array, 0, item.Buffer.Length);
+						}
+					}
 				}
 				outPut.Flush();
 			}
