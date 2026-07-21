055f3ff30bd364ba57589fda8160c2d92ef6a321
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-07-21 19:23:50 +0200

    Adding mmlink

---
+++ apps/MemLink/src/webby.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "define.h"
+#include "libc.h"
+#include "l_define.h"
+#include "l_libc.h"
+
+word MmLinkGetOpenStat(void)
+{
+    int i = 0;
+}
+
+word MmLinkTxBufClr(void)
+{
+    int i = 0;
+}
+
+word MmLinkRxBufClr(void)
+{
+    int i = 0;
+}
+
+word MmLinkPortOpen(SRL_STAT *po)
+{
+    int i = 0;
+}
+
+word MmLinkPortClose(void)
+{
+    int i = 0;
+}
+
+word MmLinkPortFClose(void)
+{
+    int i = 0;
+}
+
+word MmLinkRecvByte(byte *data)
+{
+    int i = 0;
+}
+
+word MmLinkRecvBlock(byte *data, word size, word *num)
+{
+    int i = 0;
+}
+
+word MmLinkSendBlock(byte *data, word size)
+{
+    int i = 0;
+}
+
