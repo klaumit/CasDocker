00d11e3ac4ae2d52db3b264fcaea4f9c3b1cef35
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-07-21 21:18:30 +0200

    Adding some receive and send

--- apps/MemLink/src/webby.c
+++ apps/MemLink/src/webby.c
@@ -67,18 +67,41 @@ word MmLinkPortFClose(void)
     return IW_SRL_NOERR;
 }
 
-word MmLinkRecvByte(byte *data)
-{
-    return IW_SRL_NOERR;
-}
-
 word MmLinkRecvBlock(byte *data, word size, word *num)
 {
+    word n;
+    if (!shm.rx_ready)
+    {
+        *num = 0;
+        return IW_SRL_NODATA;
+    }
+    n = (shm.rx_len < size) ? shm.rx_len : size;
+    memcpy(data, (void *)shm.rx_buf, n);
+    *num = n;
+    shm.rx_ready = 0;
     return IW_SRL_NOERR;
 }
 
 word MmLinkSendBlock(byte *data, word size)
 {
+    if (shm.tx_ready)
+    {
+        return IW_SRL_TRSERR;
+    }
+    if (size > SHM_BUF_SIZE)
+    {
+        size = SHM_BUF_SIZE;
+    }
+    memcpy((void *)shm.tx_buf, data, size);
+    shm.tx_len = size;
+    shm.tx_ready = 1;
     return IW_SRL_NOERR;
+}
+
+word MmLinkRecvByte(byte *data)
+{
+    word n;
+    MmLinkRecvBlock(data, 1, &n);
+    return (n == 1) ? IW_SRL_NOERR : IW_SRL_NODATA;
 }
 
