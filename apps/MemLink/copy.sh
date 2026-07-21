f33065b277e6c3a4bcf804678fba23f6bf94ac80
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-07-21 19:35:52 +0200

    Patching the fork

--- apps/MemLink/copy.sh
+++ apps/MemLink/copy.sh
@@ -7,6 +7,6 @@ cp ../PocLink/src/xhacks.*    ./src/
 cp ../PocLink/src/msglayer.*  ./src/
 
 sed -e "s/Pocket Link/Memory Link/g" -e "s/PocLink/MemLink/g" -e "s/15/999/g" -e "s/25/200/g" -e "s/21/180/g" ../PocLink/src/main.c > ./src/main.c
-sed -e "s/LibSrl/MmLink/g" ../PocLink/src/msglayer.c > ./src/msglayer.c
-sed -e "s/LibSrl/MmLink/g" ../PocLink/src/sysdm.c > ./src/sysdm.c
+sed -e "s/LibSrl/MmLink/g" -e 's/#include "msglayer.h"/#include "msglayer.h"\r\n#include "webby.h"/g' ../PocLink/src/msglayer.c > ./src/msglayer.c
+sed -e "s/LibSrl/MmLink/g" -e 's/#include "l_libc.h"/#include "l_libc.h"\n#include "webby.h"/g' ../PocLink/src/sysdm.c > ./src/sysdm.c
 
