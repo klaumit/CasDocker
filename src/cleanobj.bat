ea8e548f35381b2b7de7e7b6b39ebb7183d7a834
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-07-24 18:53:07 +0200

    Cleaning more

--- src/cleanobj.bat
+++ src/cleanobj.bat
@@ -2,4 +2,9 @@
 
 rd /s /q Y:\src\DevForge\obj
 rd /s /q Y:\src\DevForge.Lib\obj
+rd /s /q Y:\src\DevForge.Base\obj
+rd /s /q Y:\src\DevForge.UI\obj
+
+rd /s /q Y:\src\MemForge\obj
+rd /s /q Y:\src\MemForge.Lib\obj
 
