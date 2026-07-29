79b07324a9be17a28481e67a8e0a93fa5e7517db
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-07-29 23:14:43 +0200

    Adding range parsing

--- src/DevForge.Base/Resources/ResTool.cs
+++ src/DevForge.Base/Resources/ResTool.cs
@@ -1,6 +1,24 @@
+using System.IO;
+using System.Text;
+
 namespace DevForge.Lib.Resources
 {
 	public static class ResTool
 	{
+		public static string GetPath(string name)
+		{
+			var type = typeof(ResTool);
+			var ass = type.Assembly;
+			var dll = Path.GetFullPath(ass.Location);
+			var dir = Path.GetDirectoryName(dll);
+			var full = Path.Combine(dir, "Resources", name);
+			return full;
+		}
+
+		public static string ReadUtf(string path)
+		{
+			var text = File.ReadAllText(path, Encoding.UTF8);
+			return text;
+		}
 	}
 }
