578fffef21733232f04bbbe4f711d704605b165e
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-08-02 21:14:28 +0200

    Removing it

--- src/DevForge.UI/Views/MapForm.cs
+++ src/DevForge.UI/Views/MapForm.cs
@@ -1,22 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Windows.Forms;
 using DevForge.Lib.Hex;
-using DevForge.UI.Core;
 using DevForge.UI.Resources;
-using DevForge.UI.Tools;
-using F = System.IO.File;
-using System.ComponentModel;
 using DevForge.Lib.Visual;
 
 namespace DevForge.UI.Views
