af9ad85ec8c292530ddfd366fe4466c743cb7518
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-08-06 19:22:04 +0200

    Trying asm mode

---
+++ apps/IntFix/src/Op.c
@@ -0,0 +1,139 @@
+#include "libc.h"
+#include "Op.h"
+
+void main(void)
+{
+	volatile signed int a = 0x4A;
+    volatile unsigned int b = 0x12;
+	volatile int xa[4] = {0x2B,35,75,100};
+    print_char(a - b); /* 8 */
+
+    a = 0x1A;
+    b = 0x1D;
+    print_char(a + b); /* 7 */
+
+    a = 0x1B;
+    b = 0x02;
+    print_char(a * b); /* 6 */
+
+    a = 0x34;
+    print_char(++a); /* 5 */
+    print_char(--a); /* 4 */
+
+    a = 0x38;
+    b = 0x39;
+    print_char(a & b); /* 8 */
+
+    a = 0xAD;
+    print_char(~a); /* R */
+
+    a = 0x3C;
+    b = 0x1B;
+    print_char((a % b) + 0x30); /* 6 */
+
+    a = 0x75;
+    b = 0x03;
+    print_char((a / b) + 0x10); /* 7 */
+
+    a = 0x07;
+    b = 0x03;
+    print_char(a << b); /* 8 */
+
+    a = 0xD4;
+    b = 0x02;
+    print_char(a >> b); /* 5 */
+
+    a = 0xAB;
+    b = 0x3F;
+    print_char(a < b ? 'Y' : 'N');
+    print_char(a > b ? 'Y' : 'N');
+    print_char(a <= b ? 'Y' : 'N');
+    print_char(a >= b ? 'Y' : 'N');
+    print_char(a == b ? 'Y' : 'N');
+    print_char(a != b ? 'Y' : 'N');
+
+    a = 0xAB;
+    b = 0x3F;
+    print_char((a ^ b) - 590); /* F */
+
+    a = 0xAB;
+    b = 0x3F;
+    print_char((a | b) - 593); /* n */
+
+    print_char((a >= 10 && b >= 0) ? 'Y' : 'N');
+    print_char((a > 5776 || b < 14) ? 'Y' : 'N');
+
+    print_char((!(a > b)) ? 'Y' : 'N');
+
+	if (a >= b){
+		print_char('1'); /* 1 */
+	} else {
+		print_char('2');
+	}
+	
+	a = 0x91;
+	switch (a) {
+		case 0x91:
+			print_char('S'); /* S */
+			break;
+		default:
+			print_char('F');
+			break;
+	}
+	
+	a = 3;
+	while (a > 0) {
+		print_char('0' + (a--)); /* 3,2,1 */
+	}
+	
+	a = 4;
+	do {
+		print_char('A' + (a+=2)); /* G,I,K,M */
+	} while (a < 11);
+	
+	a = 0x1234;
+	b = 0;
+	
+	while (a) {
+		b = b * 10 + a % 10;
+		a /= 10;
+	}
+	print_char((b >> 8) + 0x30); /* 2 */
+	
+	a = 4;
+	for (a = 0; a < 8; a+=3)
+		print_char('0' + a); /* 0,3,6 */
+	
+	b = 0;
+	#ifdef _MH3
+		b = 8;
+	#endif
+
+	print_char('0'+sizeof(xa)-b); /* 8 */
+
+	b = 0;
+	#ifdef _MH3
+		b = 2;
+	#endif
+	
+	print_char('A'+sizeof(xa[1])-b); /* C */
+	xa[1] = 0x33;
+	print_char('B'+sizeof(xa[0])-b); /* D */
+	
+    exit_dos();
+}
+
+void exit_dos(void)
+{
+    nf_exit();
+}
+
+void print_nl(void)
+{
+    nf_nl();
+}
+
+void print_char(short c)
+{
+    nf_char(c);
+}
---
+++ apps/IntFix/src/Op.h
@@ -0,0 +1,51 @@
+
+#ifndef EXP_DEF
+#define EXP_DEF
+#include "define.h"
+
+void exit_dos(void);
+void print_char(short c);
+void print_nl(void);
+
+#ifdef __HITACHI__
+
+#pragma inline_asm(nf_exit)
+static int nf_exit()
+{
+    mov #76, R3
+    mov #0, R4
+    trapa #33
+}
+
+#pragma inline_asm(nf_char)
+static int nf_char(int a)
+{
+    mov #2, R3
+    trapa #33
+}
+
+#pragma inline_asm(nf_nl)
+static int nf_nl()
+{
+    mov #2, R3
+    mov #13, R4
+    trapa #33
+    mov #10, R4
+    trapa #33
+}
+
+#else
+
+#define nf_exit() _asm_nf_exit("\n mov ax, 4C00h \n int 21h \n")
+void _asm_nf_exit();
+
+#define nf_char(c) _asm_nf_char("\n mov dl, al \n mov ah, 02h \n int 21h \n", c)
+void _asm_nf_char(unsigned char *, unsigned char);
+
+#define nf_nl() _asm_nf_nl("\n mov ah, 02h \n mov dl, 0Dh \n int 21h \n mov dl, 0Ah \n int 21h \n")
+void _asm_nf_nl();
+
+#endif
+
+#endif
+
