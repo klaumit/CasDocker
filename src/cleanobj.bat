222f31c898c60e19598748d993644cc77778fac2
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-08-07 21:28:27 +0200

    Removing objs

--- src/cleanobj.bat
+++ src/cleanobj.bat
@@ -1,5 +1,10 @@
 @echo off
 
+rd /s /q Y:\src\Pva2cpa.Lib\obj
+rd /s /q Y:\src\Pva2cpa\obj
+rd /s /q Y:\src\PvMake.Lib\obj
+rd /s /q Y:\src\PvMake\obj
+
 rd /s /q Y:\src\DevForge\obj
 rd /s /q Y:\src\DevForge.Lib\obj
 rd /s /q Y:\src\DevForge.Base\obj
