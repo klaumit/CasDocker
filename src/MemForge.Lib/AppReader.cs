6b49cb238537245b5d1c83c13b788ebd205ce6d0
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-08-12 21:30:23 +0200

    Saving apps

--- src/MemForge.Lib/AppReader.cs
+++ src/MemForge.Lib/AppReader.cs
@@ -1,7 +1,10 @@
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using Hexer.Core;
+using System;
+using System.Diagnostics;
+using BW = Hexer.Wraps.BinWrap;
+using AI = Hexer.Core.AppInfo;
 using EE = Hexer.Core.ElfExt;
 using MR = MemForge.Lib.MemReader;
 
@@ -13,9 +16,11 @@ namespace MemForge.Lib
     {
         public static void WriteAllFound(uint pid, string wName)
         {
-            var bName = string.Format("proc_{0}_apps", pid);
-            if (!Directory.Exists(bName))
-                Directory.CreateDirectory(bName);
+            var localDir = string.Format("proc_{0}_apps", pid);
+            if (!Directory.Exists(localDir))
+                Directory.CreateDirectory(localDir);
+            var pvaBytes = Consts.PvaMarkB;
+            var rldBytes = Consts.RldMarkB;
             foreach (var item in MR.ReadAll(pid))
             {
                 var ii = item.Info;
@@ -23,25 +28,27 @@ namespace MemForge.Lib
                     ii.State == 0x00001000 && ii.Protect == 0x00000004 && ii.Type == 0x00020000)
                 {
                     var array = item.Buffer.SwapEndian(true);
-                    var pvaIndex = array.IndicesOf(Consts.PvaMarkB).ToArray();
-                    var rldIndex = array.IndicesOf(Consts.RldMarkB).ToArray();
-                    if (!(pvaIndex.Length >= 1 && rldIndex.Length >= 1))
+                    var pvaIdx = array.IndicesOf(pvaBytes).ToArray();
+                    var rldIdx = array.IndicesOf(rldBytes).ToArray();
+                    if (!(pvaIdx.Length >= 1 && rldIdx.Length >= 1))
                         continue;
-                    var anchors = EE.FindAnchors(pvaIndex, rldIndex).ToArray();
-                    if (!(anchors.Length >= 1))
+                    var anchors = EE.FindAnchors(pvaIdx, rldIdx).ToArray();
+                    if (anchors.Length < 1)
                         continue;
                     var baseAddr = ii.BaseAddress.ToInt64();
                     foreach (var anchor in anchors)
                     {
-                        var name = string.Format("{0:x8}_{1:D2}.pva", baseAddr, anchor.I);
-                        var full = Path.Combine(bName, name);
-
-
-                        
-                        
-                        
-
-                        Debugger.Break();
+                        var pvaName = string.Format("{0:x8}_{1:D2}.pva", baseAddr, anchor.I);
+                        var e = anchor.GetSizes(array);
+                        var ai = new AI { Name = pvaName };
+                        try
+                        {
+                            BW.ExtractFiles(ai, e.pvaSize, e.elfSize, array, anchor, localDir);
+                        }
+                        catch (ArgumentException ex)
+                        {
+                            Debug.WriteLine(ex);
+                        }
                     }
                 }
             }
