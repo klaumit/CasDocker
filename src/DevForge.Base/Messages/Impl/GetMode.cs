deeb997e13308e286a607d5e393fccef40333b86
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-08-21 00:10:55 +0200

    Adding it

--- src/DevForge.Base/Messages/Impl/GetMode.cs
+++ src/DevForge.Base/Messages/Impl/GetMode.cs
@@ -1,6 +1,8 @@
-﻿namespace DevForge.Lib.Messages.Impl
+﻿using System.Globalization;
+
+namespace DevForge.Lib.Messages.Impl
 {
-	public sealed class GetMode : BaseTxt
+    public sealed class GetMode : BaseTxt
     {
         public GetMode(string text) : base(text)
         {
@@ -10,6 +12,15 @@
         public GetMode(Message msg) : base(msg)
         {
             Kind = MsgKind.GetMode;
+        }
+
+        public void Unpack(out byte kind, out ushort code, out ushort stat, out uint ptr)
+        {
+            var gmParts = Text.Split('|');
+            kind = byte.Parse(gmParts[0], NumberStyles.HexNumber);
+            code = ushort.Parse(gmParts[1], NumberStyles.HexNumber);
+            stat = ushort.Parse(gmParts[2], NumberStyles.HexNumber);
+            ptr = uint.Parse(gmParts[3], NumberStyles.HexNumber);
         }
     }
 }
