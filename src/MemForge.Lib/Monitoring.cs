09693b08c30ac28e1fe77ab9cd8f1f3059074ca7
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-08-23 21:40:29 +0200

    Moving this

--- src/MemForge.Lib/Monitoring.cs
+++ src/MemForge.Lib/Monitoring.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.IO;
 using System.Diagnostics;
+using System.Text;
 using System.Linq;
 using MemForge.Lib;
 using B = System.BitConverter;
 using X = MemForge.Lib.ByteExt;
-using System.Text;
+using DevForge.Lib.Modern;
+using MR = MemForge.Lib.MemReader;
 
 namespace WinFinder
 {
@@ -13,17 +15,33 @@ namespace WinFinder
 	{
 		public static void ReadReg86(uint pid)
 		{
-			foreach (var mem in MemReader.ReadAll(pid))
+			foreach (var item in MR.ReadAll(pid))
 			{
-				var array = mem.Buffer;
+				if (item.Info.AllocationProtect == 0x00000080 &&
+					item.Info.State == 0x00001000 &&
+					item.Info.Protect == 0x00000004 &&
+					item.Info.Type == 0x01000000 &&
+					item.Info.RegionSize == 0x0005F000)
+				{
+					Console.WriteLine(" x86 | " + item);
+					;
+				}
 			}
 		}
 
 		public static void ReadRegSh(uint pid)
 		{
-			foreach (var mem in MemReader.ReadAll(pid))
+			foreach (var item in MR.ReadAll(pid))
 			{
-				var array = mem.Buffer;
+				if (item.Info.AllocationProtect == 0x00000080 &&
+					item.Info.State == 0x00001000 &&
+					item.Info.Protect == 0x00000004 &&
+					item.Info.Type == 0x01000000 &&
+					item.Info.RegionSize == 0x001E9000)
+				{
+					Console.WriteLine(" sh3 | " + item);
+					;
+				}
 			}
 		}
 	}
