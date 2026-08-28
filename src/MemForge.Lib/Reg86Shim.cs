cbe3ed40d4db4835f441afda98ca8de29a0a5573
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-08-29 00:26:25 +0200

    Adding it

--- src/MemForge.Lib/Reg86Shim.cs
+++ src/MemForge.Lib/Reg86Shim.cs
@@ -1,11 +1,15 @@
 using System;
 using Vanara.PInvoke;
+using System.Text;
+using DevForge.Lib.Modern;
+using MR = MemForge.Lib.MemReader;
+using SP = Vanara.PInvoke.Kernel32.SafeHPROCESS;
 
 namespace MemForge.Lib
 {
     public sealed class Reg86Shim : RegShim
     {
-        public Reg86Shim(Kernel32.SafeHPROCESS proc, IntPtr baseAddr)
+        public Reg86Shim(SP proc, IntPtr baseAddr)
             : base(proc, baseAddr)
         {
         }
--- src/MemForge.Lib/RegShShim.cs
+++ src/MemForge.Lib/RegShShim.cs
@@ -1,11 +1,15 @@
 using System;
 using Vanara.PInvoke;
+using System.Text;
+using DevForge.Lib.Modern;
+using MR = MemForge.Lib.MemReader;
+using SP = Vanara.PInvoke.Kernel32.SafeHPROCESS;
 
 namespace MemForge.Lib
 {
     public sealed class RegShShim : RegShim
     {
-        public RegShShim(Kernel32.SafeHPROCESS proc, IntPtr baseAddr)
+        public RegShShim(SP proc, IntPtr baseAddr)
             : base(proc, baseAddr)
         {
         }
