3eaca42d12627960748b57ec38854af3d6d2574e
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-08-29 22:06:29 +0200

    Adding it

---
+++ src/WinForge.Lib/MainForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinForge
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
