85abbaaecb4314133b6921fa7a3d94142e2ace3c
Klaus Mittermann <klaus.mittermann@hotmail.com>
2026-08-29 23:46:01 +0200

    Getting path

--- src/MemForge.Lib/SysExt.cs
+++ src/MemForge.Lib/SysExt.cs
@@ -1,6 +1,17 @@
+using System;
+using System.IO;
+
 namespace MemForge.Lib
 {
     public static class SysExt
     {
+        public static string GetSrcRoot()
+        {
+            var folder = Environment.CurrentDirectory;
+            var term = Path.DirectorySeparatorChar + "src";
+            var tmp = folder.Split(new[] { term }, 2, StringSplitOptions.None);
+            var full = Path.GetFullPath(tmp[0]);
+            return full;
+        }
     }
 }
